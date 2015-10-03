4adebf21f9c8feb260c59e09ee8736f63e7f4e97
xunilrj <xunilrj@hotmail.com>
2015-10-03 17:21:46 -0300

    Concrete factory for memory collections.

---
+++ MachinaAurum.Collections/MachinaAurum.Collections.Memory/MemoryCollectionsFactory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MachinaAurum.Collections.Memory
+{
+    public class MemoryCollectionsFactory : ICollectionsFactory
+    {
+        public MemoryCollectionsFactory()
+        {
+            RegisterCatalog();
+        }
+
+        public ICollection<T> Create<T>(CollectionCapabilities capabilities)
+        {
+            if(capabilities.HasFlag(CollectionCapabilities.Mutable))
+            {
+                return new MemoryArray<T>();
+            }
+
+            throw new NotImplementedException();
+        }
+             
+        private void RegisterCatalog()
+        {
+        }
+    }
+
+    public class MemoryArray<T> : IArray<T>
+    {
+        T[] Array;
+        int FirstNullPosition;
+
+        public MemoryArray()
+        {
+            Array = new T[10];
+            FirstNullPosition = 0;
+        }
+
+        T ISequenceableReader<T>.First
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        void ICollectionWriter<T>.BestInsertion(T item)
+        {
+            var pos = Interlocked.Increment(ref FirstNullPosition);
+            Array[pos - 1] = item;
+        }
+
+        T ICollectionReader<T>.BestRemove()
+        {
+            var pos = Interlocked.Decrement(ref FirstNullPosition);
+            return Array[pos + 1];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
